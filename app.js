6382f46d350c727a7eb24dd3dc1d5ae267d9976c
cheonghakim <kch7122@jongdali.com>
2021-08-28 01:18:37 +0900

    mod:method

--- app.js
+++ app.js
@@ -15,35 +15,21 @@ class Main {
       this.client.on("message", (channel, tags, message, self) => {
         if (self || !message || !this.authList.includes(tags.username)) return;
         if (message.includes("ai doya")) {
-          shell.exec("cd /home/bot & yarn start", (code, stdout, stderr) => {
-            if (stderr) {
-              this.client.say(channel, stderr);
-            } else {
-              this.client.say(channel, stdout);
-            }
-          });
+          //   shell.exec("cd /home/bot & yarn start", (code, stdout, stderr) => {
+          //     if (stderr) {
+          //       this.client.say(channel, stderr);
+          //     } else {
+          //       this.client.say(channel, stdout);
+          //     }
+          //   });
+          shell.exec("cd /home/bot");
+          shell.exec("yarn start");
         } else if (message.includes("ai rom")) {
-          shell.exec(
-            "cd /home/rom/bot & yarn start",
-            (code, stdout, stderr) => {
-              if (stderr) {
-                this.client.say(channel, stderr);
-              } else {
-                this.client.say(channel, stdout);
-              }
-            }
-          );
+          shell.exec("cd /home/rom/bot");
+          shell.exec("yarn start");
         } else if (message.includes("ai chu")) {
-          shell.exec(
-            "cd /home/chu/bot & yarn start",
-            (code, stdout, stderr) => {
-              if (stderr) {
-                this.client.say(channel, stderr);
-              } else {
-                this.client.say(channel, stdout);
-              }
-            }
-          );
+          shell.exec("cd /home/chu/bot");
+          shell.exec("yarn start");
         }
       });
     } catch (error) {
